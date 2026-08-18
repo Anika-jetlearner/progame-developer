c2116ef0b30fde058e25e7bf3bc14298a524dead
Anika-jetlearner <anika_prasad@outlook.com>
2026-08-18 18:45:57 +0200

    Add files via upload

--- flappyball.py
+++ flappyball.py
@@ -17,13 +17,23 @@ class ball:
 
 Ball=ball(50,"blue",100,100,100,0)
 Ball.draw()
+
 pygame.display.update()
 
 while True:
     changetime=clock.tick(60)/1000
     u=Ball.vy
+    Ball.x=Ball.x+Ball.vx*changetime
+    if Ball.x+Ball.radius>800 or Ball.x-Ball.radius<0:
+        Ball.vx*=-1
     Ball.vy=u+(acceleration*changetime)
     Ball.y=Ball.y+(u+Ball.vy)*0.5*changetime
+    if Ball.y+Ball.radius>600:
+        Ball.y=600-Ball.radius
+        Ball.vy=-u*0.9
+        
+    Ball.position=(Ball.x,Ball.y)
+    screen.fill("black")
     Ball.draw()
     pygame.display.update()
     print(Ball.vy)
@@ -34,5 +44,8 @@ while True:
     
 
     for i in pygame.event.get():
+        if i.type==pygame.KEYDOWN:
+            if i.key==pygame.K_SPACE:
+                Ball.vy=-500
         if i.type==pygame.QUIT:
             pygame.quit()
